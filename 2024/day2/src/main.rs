ab74a072e6f1b2780cef7c88fa829088cf444f09
Jeremie Bastien <jeremie.bastien@logmein.com>
2024-12-02 16:51:49 -0500

    better

--- 2024/day2/src/main.rs
+++ 2024/day2/src/main.rs
@@ -11,17 +11,15 @@ fn main() {
             .map(|x| x.parse().expect("could not parse"))
             .collect();
 
+        let initial_delta_is_positive = (separated[1] - separated[0]).is_positive();
         for j in 1..separated.len() {
-            if separated[j] > separated[j - 1] && (separated[j] - separated[j - 1]).abs() <= 3 {
-                if j == separated.len() - 1 {
-                    safe_lines += 1
-                }
-            } else {
-                break;
-            }
-        }
-        for j in 1..separated.len() {
-            if separated[j] < separated[j - 1] && (separated[j] - separated[j - 1]).abs() <= 3 {
+            let delta = separated[j] - separated[j - 1];
+            let less_than_three_delta = delta.abs() <= 3;
+            let delta_is_positive = delta.is_positive();
+            let delta_same_sign_than_initial_delta = initial_delta_is_positive == delta_is_positive;
+            let delta_non_zero = delta != 0;
+
+            if delta_same_sign_than_initial_delta && delta_non_zero && less_than_three_delta {
                 if j == separated.len() - 1 {
                     safe_lines += 1
                 }
@@ -30,5 +28,6 @@ fn main() {
             }
         }
     }
+    assert!(safe_lines == 359);
     print!("{}", safe_lines)
 }
