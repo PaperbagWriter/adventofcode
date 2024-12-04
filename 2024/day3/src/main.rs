7654ce595885631a7d360852c02465c50fb161ed
Jeremie Bastien <jeremie.bastien@logmein.com>
2024-12-03 20:03:07 -0500

    complete day3

---
+++ 2024/day3/src/main.rs
@@ -0,0 +1,52 @@
+mod input;
+
+fn main() {
+    let input = input::get_input();
+    part1(&input);
+    part2(&input);
+}
+
+fn sum_multiplications(input: &str) -> i32 {
+    let mut result: i32 = 0;
+
+    let regex_splitted: Vec<&str> = input.split("mul(").collect();
+    for i in 0..regex_splitted.len() {
+        let sub = regex_splitted[i];
+        let sub: Vec<&str> = sub
+            .split_terminator(')')
+            .filter(|x| x.contains(','))
+            .collect();
+        for i in 0..sub.len() {
+            let (str1, str2) = match sub[i].split_once(',') {
+                Some(a) => (a.0.parse::<i32>(), a.1.parse::<i32>()),
+                None => todo!(),
+            };
+            match (str1, str2) {
+                (Ok(str1), Ok(str2)) => {
+                    result += str1 * str2;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    return result;
+}
+
+fn part1(input: &str) {
+    let result = sum_multiplications(input);
+    println!("{}", result);
+}
+
+fn part2(input: &str) {
+    let mut result = 0;
+    let mut input = input;
+    while input.contains("don't()") {
+        let (subs, rest) = input.split_at(input.find("don't()").unwrap());
+        result += sum_multiplications(subs);
+        let (_, rest) = rest.split_at(rest.find("do()").unwrap() + 4);
+        input = rest;
+    }
+    result += sum_multiplications(input);
+    println!("{}", result)
+}
